864deb1e4e6afc0c0a9c09e350616daaaf0eae74
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-08-16 16:01:39 -0500

    Started creating add song form

--- musiclibrary/src/components/SongForm/SongForm.jsx
+++ musiclibrary/src/components/SongForm/SongForm.jsx
@@ -3,12 +3,30 @@ import React, { Component } from 'react'
 class SongForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      title: '',
+      album: '',
+      artist: '',
+      release_date: '',
+    }
   }
+
   render() { 
     return ( 
-      <h1>Hola from SongForm lmaoooo</h1>
-     );
+      <div>
+        <h1>Please add a new song to the playlist. Lmaooo</h1>
+        <form action="" method="post">
+          <label for="title">Title: </label>
+          <input type="text" name="title"><br>
+          <label for="album">Album: </label>
+          <input type="text" name="album"><br>
+          <label for="artist">Artist: </label>
+          <input type="text" name="artist"><br>
+          <label for="release_date">Release Date: </label>
+          <input type="date" name="release_date">
+        </form>
+      </div>
+    );
   }
 }
  
